391cd178c9d04b106007b6b914e78a82aec47e50
Emad Al-Din Hamza <emad.aldin.hamza@gmail.com>
2022-02-25 21:30:22 +0200

    fixed migrations

--- migrations/Version20220225192805.php
+++ migrations/Version20220225192805.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220224165546 extends AbstractMigration
+final class Version20220225192805 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,15 +20,14 @@ final class Version20220224165546 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP SEQUENCE admin_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE user_id_seq CASCADE');
         $this->addSql('CREATE SEQUENCE image_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE tag_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE image (id INT NOT NULL, name VARCHAR(255) NOT NULL, provider VARCHAR(255) NOT NULL, external_url TEXT DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE image (id INT NOT NULL, name VARCHAR(255) DEFAULT NULL, provider VARCHAR(255) NOT NULL, external_url TEXT DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE image_tag (image_id INT NOT NULL, tag_id INT NOT NULL, PRIMARY KEY(image_id, tag_id))');
         $this->addSql('CREATE INDEX IDX_5B6367D03DA5256D ON image_tag (image_id)');
         $this->addSql('CREATE INDEX IDX_5B6367D0BAD26311 ON image_tag (tag_id)');
         $this->addSql('CREATE TABLE tag (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_389B7835E237E06 ON tag (name)');
         $this->addSql('ALTER TABLE image_tag ADD CONSTRAINT FK_5B6367D03DA5256D FOREIGN KEY (image_id) REFERENCES image (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE image_tag ADD CONSTRAINT FK_5B6367D0BAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
@@ -41,8 +40,6 @@ final class Version20220224165546 extends AbstractMigration
         $this->addSql('ALTER TABLE image_tag DROP CONSTRAINT FK_5B6367D0BAD26311');
         $this->addSql('DROP SEQUENCE image_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE tag_id_seq CASCADE');
-        $this->addSql('CREATE SEQUENCE admin_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE user_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('DROP TABLE image');
         $this->addSql('DROP TABLE image_tag');
         $this->addSql('DROP TABLE tag');
